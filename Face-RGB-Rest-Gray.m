0778e3070c6a050e61a5967c92f488bae7030db8
Optimist Jamil <92690062+Jamil-Ahmed808@users.noreply.github.com>
2021-10-20 12:43:27 +0500

    Create Face-RGB-Rest-Gray.m

---
+++ Face-RGB-Rest-Gray.m
@@ -0,0 +1,11 @@
+img = imread('JAMIL AHMED.jpg');
+FD = vision.CascadeObjectDetector('MergeThreshold', 10);
+bbox=step(FD,img);
+for k = 1 : size(bbox, 1);
+    crop{i} = imcrop(img, bbox(i, :));
+end
+img = rgb2gray(img);
+img = cat(3, img, img, img);
+for k = 1 : size(crop,2)
+    img(bbox(i,2): bbox(i,2)+bbox(i,4), bbox(i,1): bbox(i,1)+bbox(i,3),:)=crop{i};
+end
